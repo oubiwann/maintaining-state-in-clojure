77c2a0a2396632ddbd58bf6c7cd881635ae0680f
Duncan McGreggor <oubiwann@ubuntu.com>
2013-11-09 17:16:13 -0600

    Updated project.clj.

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/oubiwann/maintaining-state-in-clojure"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {
+    :dev {
+      :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                     [org.clojure/java.classpath "0.2.0"]]}
+    :testing {
+      :dependencies [[leiningen "2.3.3"]]}})
