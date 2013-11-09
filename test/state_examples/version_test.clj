5c0051a374d7382385a8818aeeeddaee919b0d3e
Duncan McGreggor <oubiwann@ubuntu.com>
2013-11-09 17:35:35 -0600

    Added unit tests for version module.

---
+++ test/state_examples/version_test.clj
@@ -0,0 +1,13 @@
+(ns state-examples.version-test
+  (:require [clojure.test :refer :all]
+            [state-examples.version :as version]))
+
+
+(deftest test-version
+  (is (= 0 (version/version :major)))
+  (is (= 1 (version/version :minor)))
+  (is (= 0 (version/version :patch)))
+  (is (= true (version/version :snapshot))))
+
+(deftest test-version-str
+  (is (= "v0.1-dev" version/version-str)))
