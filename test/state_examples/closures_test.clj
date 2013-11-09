a12e849e38e5f0243e5f91d5eaaf0e0c8c2fe5c9
Duncan McGreggor <oubiwann@ubuntu.com>
2013-11-09 17:47:17 -0600

    Added unit tests for closures example.

---
+++ test/state_examples/closures_test.clj
@@ -0,0 +1,17 @@
+(ns state-examples.closures-test
+  (:require [clojure.test :refer :all]
+            [state-examples.closures :as closures]))
+
+
+(deftest test-account
+  (let [account (closures/new-account "savings" 1000 0.05)]
+    (testing "check account name"
+      (is (= "savings" (closures/get-name account))))
+    (testing "heck account balance"
+      (is (= 1000 (closures/get-balance account))))
+    (testing "check account interest"
+      (is (= 1050.0 (closures/get-balance (closures/apply-interest account)))))
+    (testing "check account withdraw"
+      (is (= 860 (closures/get-balance (closures/withdraw account 140)))))
+    (testing "check account deposit"
+      (is (= 3500 (closures/get-balance (closures/deposit account 2500)))))))
