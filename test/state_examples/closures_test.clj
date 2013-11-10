1e126af98a9742f5c7d852c7831b42edc88176a3
Duncan McGreggor <oubiwann@ubuntu.com>
2013-11-09 19:40:14 -0600

    Added a test for overdrawing.

--- test/state_examples/closures_test.clj
+++ test/state_examples/closures_test.clj
@@ -13,5 +13,7 @@
       (is (= 1050.0 (closures/get-balance (closures/apply-interest account)))))
     (testing "check account withdraw"
       (is (= 860 (closures/get-balance (closures/withdraw account 140)))))
+    (testing "check account over-withdraw"
+      (is (thrown? java.lang.Exception (closures/withdraw account 1000.01))))
     (testing "check account deposit"
       (is (= 3500 (closures/get-balance (closures/deposit account 2500)))))))
